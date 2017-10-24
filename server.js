3bfae54edfb4c25676c24863691ad9170b4b1c4f
Jordi López <jordi.lopez@softonic.com>
2017-10-24 18:04:59 +0200

    third commit

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 require("marko-starter").projectConfig({
   lassoConfig: {
-      bundlingEnabled: isProduction,
-      fingerprintsEnabled: isProduction,
-      require: {
-        // ...
-      },
-      minifyJS: false,
-      plugins: [
-          'lasso-marko',
-          'lasso-sass'
-      ]
+    bundlingEnabled: isProduction,
+    fingerprintsEnabled: isProduction,
+    require: {
+      // ...
+    },
+    minifyJS: false,
+    plugins: [
+        'lasso-marko',
+        'lasso-sass'
+    ]
   }
 }).server({
   httpPort: process.env.PORT || 8080
